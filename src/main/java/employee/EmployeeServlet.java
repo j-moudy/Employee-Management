3357b7166f8d62fbf100c6f451760398f8748499
Jeremy Moudy <jeremy.moudy@sjsu.edu>
2022-02-02 11:10:14 -0800

    Add switch statement to facilitate future features

--- src/main/java/employee/EmployeeServlet.java
+++ src/main/java/employee/EmployeeServlet.java
@@ -25,18 +25,26 @@ public class EmployeeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
 
-        String action = request.getServletPath();
-        System.out.println(action);
-        // Get the current session information to be used for sending response back to the front end
-        HttpSession session = request.getSession();
-
         // Parameters received from the font end
         int id = Integer.parseInt(request.getParameter("empID"));
         String name = request.getParameter("empName");
         double salary = Double.parseDouble(request.getParameter("empSalary"));
-
         Employee employee = new Employee(id, name, salary);
-        employeeDAO.insertEmployee(employee);
+
+        String action = request.getServletPath();
+        System.out.println(action);
+        switch (action) {
+            case "/employee/add":
+                employeeDAO.insertEmployee(employee);
+                break;
+            case "/employee/delete":
+                employeeDAO.deleteEmployee(employee);
+                break;
+
+        }
+        // Get the current session information to be used for sending response back to the front end
+        HttpSession session = request.getSession();
+
     }
 
 
